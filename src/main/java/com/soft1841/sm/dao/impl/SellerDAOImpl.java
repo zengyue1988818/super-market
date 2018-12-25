07ace95e2b8b3463c526b52811794abf9f8f8241
曾月1988818 <2414691160@qq.com>
2018-12-25 19:59:34 +0800

    我的个人仓库

---
+++ src/main/java/com/soft1841/sm/dao/impl/SellerDAOImpl.java
@@ -0,0 +1,23 @@
+package com.soft1841.sm.dao.impl;
+
+import cn.hutool.db.Db;
+import cn.hutool.db.Entity;
+import com.soft1841.sm.dao.SellerDAO;
+import com.soft1841.sm.entity.Seller;
+
+import javax.swing.text.html.parser.Entity;
+import java.sql.SQLException;
+
+public class SellerDAOImpl implements SellerDAO {
+
+    @Override
+    public Seller getSellerByNumber(String number) throws SQLException {
+        Entity entity =  Db.use().queryOne("SELECT * FROM t_cashier WHERE number = ? ",number );
+        return convertSeller(entity);
+    }
+    private Seller convertSeller(Entity entity){
+        Seller seller = new Seller(entity.getLong("id"),entity.getStr("number"),
+                entity.getStr("password"),entity.getStr("name"));
+        return  seller;
+    }
+}
